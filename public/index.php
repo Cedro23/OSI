68633ccfff471a7f9bfa39fea0359c4c12421606
Cedro <cedric1lesueur@gmail.com>
2018-05-25 12:51:23 +0200

    Modif de la bible + début du routeur

--- public/index.php
+++ public/index.php
@@ -1,5 +1,19 @@
 <?php
     require_once("../src/class.php");
+    $url = '';
+    if (isset($_GET['url'])) {
+        $url = explode('/',$_GET['url']);
+    }
+
+    if ($url == '') {
+        print('Page d\'accueil');
+    } elseif ($url[0] == 'list') {
+        print('List des profils');
+    } elseif ($url[0] == 'profil') {
+        print('Profils');
+    } else {
+        print('Error 404');
+    }
  ?>
 
 <!DOCTYPE html>
@@ -11,6 +25,7 @@
     <body>
         <div class="">
             <h1>CA MAAAAAAAAAAAAAAARCHE</h1>
+            <a href="../src/list.php">Oyo</a>
         </div>
     </body>
 </html>
---
+++ src/list.php
@@ -0,0 +1,15 @@
+<?php
+    require_once("class.php");
+    require_once("functions.php");
+    
+ ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+
+    </body>
+</html>
