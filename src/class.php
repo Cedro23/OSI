0fbc9977254d50df0ec96de762e41b9a673cd0ad
Cedro <cedric1lesueur@gmail.com>
2018-05-29 16:48:24 +0200

    Ajout du composer, modif de la page profil et de la classe profil

--- src/class.php
+++ src/class.php
@@ -1,4 +1,5 @@
 <?php
     require_once("class/Connection.php");
+    require_once("class/Profil.php");
     $connection = new Connection('mysql:dbname=osi;host=127.0.0.1', 'root', '');
  ?>
--- src/class/Profil.php
+++ src/class/Profil.php
@@ -4,10 +4,25 @@
      */
     class Profil
     {
+        private $id;
+        private $title;
+        private $contract;
+        private $class;
+        private $formation;
+        private $description;
+        private $period;
+        private $skills;
 
-        function __construct()
+        function __construct($_id, $_title, $_contract, $_class, $_formation, $_description, $_period, $_skills)
         {
-
+            $this->id = $_id;
+            $this->title = $_title;
+            $this->contract = $_contract;
+            $this->class = $_class;
+            $this->formation = $_formation;
+            $this->period = $_period;
+            $this->description = $_description;
+            $this->skills = $_skills;
         }
     }
 
