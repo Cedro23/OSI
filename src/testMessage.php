0bd7ca74ba098532b9250a4221b0ddba4cd2a1f2
Cedro <cedric1lesueur@gmail.com>
2018-05-30 14:47:49 +0200

    2 mails envoyés au lieu d'un

--- src/testMessage.php
+++ src/testMessage.php
@@ -10,13 +10,24 @@ $transport = (new Swift_SmtpTransport('smtp.gmail.com', 465, 'ssl'))
 $mailer = new Swift_Mailer($transport);
 
 // Create a message
-$message = (new Swift_Message('CA MAAAAARCHEEEEE'))
+$messageToYnov = (new Swift_Message('Interet pour offre [Nom de l\'offre]'))
 ->setFrom(['stageynovB1@gmail.com' => 'Stage Ynov'])
-->setTo(['cedric1lesueur@gmail.com' => 'Cedric Lesueur'])
-->setBody('FAZDAFAJ¨VN¨N¨JAF AFOE¨P Ä DJ ADAOIFOSQUBF AFBAGKSBD LUADSDAUH FBAIU SODN LNNJALJLIBFLAUZ ODHSQVHZERFOQYJMWXN AIYFBOZEYVFSQMDN LQUSBVCzbiuHCI IUIQBV QOI')
+->setTo(['stageynovB1@gmail.com' => 'Stage Ynov'])
+->setBody('Bonjour,
+Je suis interessé par [Nom de l\'offre]. Pour plus d\'information vous pouvez me contacter au [Numéro de téléphone].
+Bien à vous,
+[Nom de l\'envoyeur]')
+;
+
+$messageToSender = (new Swift_Message('Demande de contact YnovStage'))
+->setFrom(['stageynovB1@gmail.com' => 'Stage Ynov'])
+->setTo(['cedric1lesueur@gmail.com' => 'Cédric Lesueur'])
+->setBody('Bonjour,
+Votre demande a bien été prise en compte.')
 ;
 
 // Send the message
-$result = $mailer->send($message);
+$result = $mailer->send($messageToYnov);
+$result = $mailer->send($messageToSender);
 
 ?>
