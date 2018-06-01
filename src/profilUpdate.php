340a393532e87eebe9fd025aa40fb00711471c25
redwappin <mimili59.didine@hotmail.fr>
2018-06-01 10:33:19 +0200

    metaprofil

--- src/profilUpdate.php
+++ src/profilUpdate.php
@@ -3,6 +3,7 @@
 
     $url =  "//{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}";
 
+
 ?>
 <!doctype html>
 <html lang="en">
@@ -14,20 +15,29 @@
 
       gtag('config', 'UA-119962245-1');
     </script>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="author" content="">
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-119962245-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
 
+      gtag('config', 'UA-119962245-1');
+    </script>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta name="robots" content="noodp"/>
     <link rel="canonical" href=<?php print($url) ?> />
     <meta property="og:type" content="website">
-    <meta property="og:title" content="Listes des offres de Stage/CDI/Alternance par type de formation ">
-    <meta property="og :description" content="Liste des offres avec possibilité de filtrage par type de contrat(Stage,CDI,Alternance, Contrat professionnel) par année(B1,B2, B3, M1, M2) et par compétence selon la formation de l'étudiant. Recrutez le profil d'étudiant que vous cherchez">
+    <meta property="og:title" content=<?php print( "Modifiez l'offre:".$profils[$idProfil]->getTitle()) ?>>
+    <meta property="og:description" content=<?php print($description) ?>>
     <meta property="og:url"  content=<?php print($url)?>>
     <meta property="og:locale" content="fr-FR">
     <script type="text/javascript" async src=https://www.google-analytics.com/analytics.js></script>
     <script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js"></script>
 
-    <title>Liste des offres</title>
+    <title><?php print($title) ?></title>
 
     <!-- CSS -->
     <link rel="stylesheet" href="../css/style.css"/>
